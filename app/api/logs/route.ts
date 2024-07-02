c7ff07bd8a49d41e23ac8f61a25026c899650f86
kietdev1 <124799849+kietdev1@users.noreply.github.com>
2024-07-02 06:46:42 +0000

    Update

--- app/api/logs/route.ts
+++ app/api/logs/route.ts
@@ -1,37 +1,32 @@
 import { connectToDatabase } from '@/lib/mongodb/mongodb'
 import Log from '@/lib/types/Log';
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextResponse } from 'next/server';
 
 const { MONGODB_COLLECTION } = process.env
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'GET') {
-        try {
-            const { db } = await connectToDatabase();
+export async function GET(request: Request) {
+    try {
+        const { db } = await connectToDatabase();
 
-            const logs = await db.collection<Log>(MONGODB_COLLECTION ?? 'Logs')
-                .find({})
-                .sort({ Timestamp: -1 })
-                .limit(25)
-                .map((doc) => {
-                    if (doc.MessageTemplate) {
-                        doc.Message = doc.MessageTemplate?.replace('{@Environment}', doc.Properties?.Environment ?? '')
-                            ?.replace('{@ServiceName}', doc.Properties?.ServiceName ?? '')
-                            ?.replace('{@EventName}', doc.Properties?.EventName ?? '')
-                            ?.replace('{@Description}', doc.Properties?.Description ?? '')
-                            ;
-                    }
+        const logs = await db.collection<Log>(MONGODB_COLLECTION ?? 'Logs')
+            .find({})
+            .sort({ Timestamp: -1 })
+            .limit(25)
+            .map((doc) => {
+                if (doc.MessageTemplate) {
+                    doc.Message = doc.MessageTemplate?.replace('{@Environment}', doc.Properties?.Environment ?? '')
+                        ?.replace('{@ServiceName}', doc.Properties?.ServiceName ?? '')
+                        ?.replace('{@EventName}', doc.Properties?.EventName ?? '')
+                        ?.replace('{@Description}', doc.Properties?.Description ?? '')
+                        ;
+                }
 
-                    return doc;
-                })
-                .toArray();
+                return doc;
+            })
+            .toArray();
 
-            res.status(200).json(logs)
-        } catch (error) {
-            res.status(500).json({ success: false, error: 'Error fetching logs' })
-        }
-    } else {
-        res.setHeader('Allow', ['GET'])
-        res.status(405).end(`Method ${req.method} Not Allowed`)
+        return NextResponse.json(logs)
+    } catch (error) {
+        return NextResponse.json({ success: false, error: 'Error fetching logs' })
     }
 }
